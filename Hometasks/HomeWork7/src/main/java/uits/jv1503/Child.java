7ec078a7ca9c4df93b3b9c144a3fe5cd7945b5f8
Lucky <lucky526282@gmail.com>
2015-12-03 18:29:14 +0200

    ChildParent git commit -m ChildParent

---
+++ Hometasks/HomeWork7/src/main/java/uits/jv1503/Child.java
@@ -0,0 +1,19 @@
+package uits.jv1503;
+
+/**
+ * Created by Lucky on 03.12.2015.
+ */
+public class Child {
+    int c ;
+    int d;
+    {
+        d =33;
+    }
+    static int b;
+    static {
+        b =15;
+    }
+    public Child (){
+        c =100;
+    }
+}
---
+++ Hometasks/HomeWork7/src/main/java/uits/jv1503/Parent.java
@@ -0,0 +1,19 @@
+package uits.jv1503;
+
+/**
+ * Created by Lucky on 03.12.2015.
+ */
+public class Parent {
+    int a ;
+    int d;
+    {
+        d =77;
+    }
+    static int b;
+   static {
+       b =10;
+    }
+    public Parent (){
+        a = 157;
+    }
+}
